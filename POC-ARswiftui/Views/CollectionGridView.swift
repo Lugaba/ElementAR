5cff4ad601b180ea541b78abc370c41b2e2a025a
caroltaus <carolinetaus@hotmail.com>
2022-06-08 13:55:10 -0300

    Organização e botão de abrir conquistas

---
+++ POC-ARswiftui/Views/CollectionGridView.swift
@@ -0,0 +1,62 @@
+//
+//  CollectionGridView.swift
+//  POC-ARswiftui
+//
+//  Created by Caroline Taus on 08/06/22.
+//
+
+import SwiftUI
+
+struct CollectionGridView: View {
+    
+    let elements: [CellContent] = [CellContent(color: Color.redElementar, name: "Fogo", icon: Image("fireIcon")),
+                                   CellContent(color: Color.blueElementar, name: "Água", icon: Image("waterIcon")),
+                                   CellContent(color: Color.yellowElementar, name: "Ar", icon: Image("airIcon")),
+                                   CellContent(color: Color.greenElementar, name: "Terra", icon: Image("earthIcon")),
+                                   CellContent(color: Color.redElementar, name: "Lava", icon: Image("lavaIcon")),
+                                   CellContent(color: Color.yellowElementar, name: "Pó", icon: Image("dustIcon")),
+                                   CellContent(color: Color.greenElementar, name: "Lama", icon: Image("mudIcon")),
+                                   CellContent(color: Color.yellowElementar, name: "Energia", icon: Image("energyIcon")),
+                                   CellContent(color: Color.blueElementar, name: "Chuva", icon: Image("rainIcon")),
+                                   CellContent(color: Color.blueElementar, name: "Vapor", icon: Image("vaporIcon"))
+    ]
+    let columns = [
+        GridItem(.adaptive(minimum: 200)),
+        GridItem(.adaptive(minimum: 200)),
+    ]
+    var body: some View {
+        TabView {
+            VStack {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(Array(elements[0...5])) { element in
+                        CollectionCellView(color: element.color, name: element.name, icon: element.icon, isLocked: false)
+                    }
+                }
+            }
+            VStack {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(Array(elements[6...9])) { element in
+                        CollectionCellView(color: element.color, name: element.name, icon: element.icon, isLocked: false)
+                    }
+                    
+                    Rectangle().fill(.clear)
+                        .frame(width: 120, height: 120)
+                    Rectangle().fill(.clear)
+                        .frame(width: 120, height: 120)
+                }
+            }
+        }
+        .tabViewStyle(.page(indexDisplayMode: .always))
+        .background(Color.blackElementar)
+        
+        
+        
+        
+    }
+}
+
+struct CollectionGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        CollectionGridView()
+    }
+}
