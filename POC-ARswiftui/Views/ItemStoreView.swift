4a0be01f1e9bd9eb16973ea24dd0a171f137c935
Luca Hummel <luca.hummel12@gmail.com>
2022-06-22 14:06:07 -0300

    StoreView done

--- POC-ARswiftui/Views/ItemStoreView.swift
+++ POC-ARswiftui/Views/ItemStoreView.swift
@@ -8,45 +8,66 @@
 import SwiftUI
 
 struct ItemStoreView: View {
+    private var name: String
+    private var imageName: String
+    private var price: Float
+    private var description: String
+    private var pdfName: String
+    
+    init(name: String, imageName: String, price: Float, description: String, pdfName: String) {
+        self.name = name
+        self.imageName = imageName
+        self.price = price
+        self.description = description
+        self.pdfName = pdfName
+    }
+    
     var body: some View {
         ZStack(alignment: .leading) {
             Color.white.ignoresSafeArea()
             VStack(alignment: .leading) {
                 HStack(spacing: 16) {
-                    Image("fireTest")
+                    Image(imageName)
                         .resizable()
                         .aspectRatio(contentMode: .fill)
-                        .frame(width: 120, height: 120)
+                        .frame(width: 130, height: 130)
                         .clipped()
                         .cornerRadius(10)
                     VStack(alignment: .leading, spacing: 16) {
-                        Text("Bom dia").foregroundColor(.black)
-                            .font(.title)
+                        Text(name).foregroundColor(.black)
+                            .font(.title2)
                             .fontWeight(.semibold)
-                        Text("Pacote de 50 cartamnfjdbasjfbjdfajkbfas!")
+                        Text(description)
                             .foregroundColor(.black)
                             .font(.caption)
                             .fixedSize(horizontal: false, vertical: true)
-                        NavigationLink(destination: PDFUIView(name: "freeCards")) {
-                            Text("Comprar").padding(8).foregroundColor(.white)
+                        HStack {
+                            Spacer()
+                            NavigationLink(destination: PDFUIView(name: pdfName)) {
+                                Text("R$ \(price, specifier: "%.2f")").padding(8).foregroundColor(.white)
+                            }
+                            .background(Color.green)
+                            .cornerRadius(10)
                         }
-                        .background(Color.green)
-                        .cornerRadius(10)
+                        
                     }
                 }.frame(alignment: .leading)
-            }.padding()
+            }.padding(8)
 
         }
         .frame(height: 150)
         .cornerRadius(10)
-        .padding()
         .navigationTitle("Store")
         .navigationBarTitleDisplayMode(.inline)
+        .padding(.trailing, 8)
+        .padding(.leading, 8)
+        .padding(.top, 8)
+
     }
 }
 
 struct ItemStoreView_Previews: PreviewProvider {
     static var previews: some View {
-        ItemStoreView()
+        ItemStoreView(name: "Free", imageName: "fire", price: 10, description: "Muito bacana", pdfName: "freeCards")
     }
 }
--- POC-ARswiftui/Views/StoreView.swift
+++ POC-ARswiftui/Views/StoreView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct StoreView: View {
-    let pacotes = [0, 1, 2, 3]
+    let pacotes: [PackageContent] = [PackageContent(name: "4 cartas grátis", imageName: "fire", price: 0, description: "Descubra os elementos a partir da água, terra, fogo e ar!", pdfName: "freeCards"), PackageContent(name: "Cartas premium", imageName: "water", price: 10, description: "Compre o pacote premium de 25 cartas e descubra todos os elementos do jogo", pdfName: "freeCards")]
     
     var body: some View {
         VStack(spacing: 0) {
             ScrollView {
-                Text("Você precisa de uma impressora")
-                ForEach((1...10).reversed(), id: \.self) { _ in
-                    ItemStoreView().background(.purple)
+                Text("⚠️ Você precisa de uma impressora ⚠️")
+                    .padding()
+                    .background(.yellow.opacity(0.8))
+                    .cornerRadius(15)
+                    .padding(.top, 20)
+                    
+                ForEach(Array(pacotes)) { pacote in
+                    ItemStoreView(name: pacote.name, imageName: pacote.imageName, price: pacote.price, description: pacote.description, pdfName: pacote.pdfName)
                 }
             }
         }
